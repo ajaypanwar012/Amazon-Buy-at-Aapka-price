6bad2e88a9d222259d39484d61d26a074f2e839c
Ajay Panwar <ajaypanwar012@gmail.com>
2022-04-21 21:06:44 +0530

    Added button for "buy at this price" and few validation checks

--- js/app.js
+++ js/app.js
@@ -42,3 +42,49 @@ function goUp()
     });
 }
 
+function getCustomerMonitorPrice()
+{
+    let currentPrice = Number(document.getElementById("currentMonitorPrice").innerText);
+    let customerPrice = Number(document.getElementById("customerMonitorPrice").value);
+
+    console.log(currentPrice);
+    console.log(customerPrice);
+    
+    if(customerPrice == 0)
+    {
+        document.getElementById("postMonitorPriceSubmit").style.color = "orange";
+        document.getElementById("postMonitorPriceSubmit").innerHTML = "Please set a price, at which you want to buy!";
+    }
+    else if(currentPrice <= customerPrice){
+        document.getElementById("postMonitorPriceSubmit").style.color = "red";
+        document.getElementById("postMonitorPriceSubmit").innerHTML = "Please set a price lower than current price!";
+    }
+    else{
+        document.getElementById("postMonitorPriceSubmit").style.color = "green";
+        document.getElementById("postMonitorPriceSubmit").innerHTML = "We'll buy this product for you at or less than: $ " + customerPrice;
+    }
+}
+
+function getCustomerTapePrice()
+{
+    let currentPrice = Number(document.getElementById("currentTapePrice").innerText);
+    let customerPrice = Number(document.getElementById("customerTapePrice").value);
+
+    console.log(currentPrice);
+    console.log(customerPrice);
+
+    if(customerPrice == 0)
+    {
+        document.getElementById("postTapePriceSubmit").style.color = "orange";
+        document.getElementById("postTapePriceSubmit").innerHTML = "Please set a price, at which you want to buy!";
+    }
+    else if(currentPrice <= customerPrice){
+        document.getElementById("postTapePriceSubmit").style.color = "red";
+        document.getElementById("postTapePriceSubmit").innerHTML = "Please set a price lower than current price!";
+    }
+    else{
+        document.getElementById("postTapePriceSubmit").style.color = "green";
+        document.getElementById("postTapePriceSubmit").innerHTML = "We'll buy this product for you at or less than: $ " + customerPrice;
+    }
+}
+
